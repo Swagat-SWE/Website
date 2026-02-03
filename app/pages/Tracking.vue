924012a6523f54a61732ee3117023c0ee97a9dc9
walzy44 <ew562481@loras.edu>
2026-02-02 18:19:55 -0600

    C49025A-367

--- app/pages/Tracking.vue
+++ app/pages/Tracking.vue
@@ -1,7 +1,38 @@
 <template>
   <div>
     <h1>Tracking Page</h1>
+    <h2>Thank you for your order!</h2>
+    <h3>Order #abc123 placed</h3>
   </div>
+  <table>
+    <tr>
+      <td>Review Cart</td>
+    </tr>
+    <tr>
+      Item one
+    </tr>
+    <tr>
+      Item two
+    </tr>
+    <tr>
+      Item three
+    </tr>
+    <tr>
+      Track Order  
+    </tr>
+    <tr>
+      Put images here
+    </tr>
+    <tr>
+      <td>Recived</td>
+      <td>In Progress</td>
+      <td>Ready for Pickup!</td>
+    </tr>
+    <tr>
+      Once food is ready head to pickup counter!
+    </tr>
+  </table>
+  
 </template>
 
 <script setup>
