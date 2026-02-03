e32c509b225c2e450b4be0a9f971a78bfd3d926a
walzy44 <ew562481@loras.edu>
2026-02-02 19:03:02 -0600

    C49025A-367

--- app/pages/Tracking.vue
+++ app/pages/Tracking.vue
@@ -24,16 +24,32 @@
       <td>Put images here</td>
     </tr>
     <tr>
-      <td>Recived</td>
+      <td>Received</td>
       <td>In Progress</td>
       <td>Ready for Pickup!</td>
     </tr>
-    <tr>
-      Once food is ready head to pickup counter!
-    </tr>
   </table>
+  <div>
+    Once food is ready head to pickup counter
+  </div>
   
 </template>
 
 <script setup>
 </script>
+<style scoped>
+/* Einstein-ish warm palette */
+:root {
+  --cream: #f6f0e8;
+  --cream2: #fbf8f3;
+  --brown: #4b3429;
+  --brown2: #6a4a3a;
+
+  /* Your plus button orange */
+  --orange: #f4a51c;
+  --orangeHover: #ffb42f;
+
+  --yellow: #f4b316;
+  --cardShadow: 0 10px 30px rgba(0, 0, 0, 0.08);
+}
+</style>
