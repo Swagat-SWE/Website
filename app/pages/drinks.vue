56a87050fd876541c962ac7400d8dce49598a10b
Ryan McGrath <ryan.mcgrath@loras.edu>
2026-04-13 18:30:02 -0500

    C49025A-292

--- app/pages/drinks.vue
+++ app/pages/drinks.vue
@@ -274,7 +274,7 @@
           </li>
         </ul>
 
-        <button class="checkoutBtn" type="button" @click="goToCheckout">Checkout</button>
+        <button class="checkoutBtn" type="button" :disabled="cart.length === 0" @click="goToCheckout">Checkout</button>
       </aside>
     </main>
   </div>
@@ -1091,6 +1091,12 @@ const bottledDrinks = ref([
 }
 .checkoutBtn:hover {
   background: #ffbe21;
+}
+
+.checkoutBtn:disabled {
+  background: #ccc;
+  color: #666;
+  cursor: not-allowed;
 }
 
 .cartList {
